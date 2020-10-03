076b994a996f814eced4fbfdc63a10a1fdc38f22
Bohdan Cherchyk <bocherch@microsoft.com>
2020-10-03 17:07:05 -0400

    yaml

--- kubernetes_volume_mounts/src/local_test.cmd
+++ kubernetes_volume_mounts/src/local_test.cmd
@@ -3,3 +3,8 @@ docker rm volmountjob
 docker build -t volmountjob -f job.dockerfile .
 docker volume create --name mntazure
 docker run -it --name volmountjob -v mntazure:/mnt/azure volmountjob
+
+#bohdan
+az acr login --name acrdocs
+docker tag volmountjob acrdocs.azurecr.io/volmountjob:v1
+docker push acrdocs.azurecr.io/volmountjob:v1
